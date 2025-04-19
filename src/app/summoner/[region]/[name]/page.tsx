e70d401198f3646a8bfef2f22d4b2dd53374cd8a
Denis <f104378@students.nbu.bg>
2025-04-19 15:54:13 +0300

    summoner icon fix

--- src/app/summoner/[region]/[name]/page.tsx
+++ src/app/summoner/[region]/[name]/page.tsx
@@ -74,12 +74,12 @@ export default function SummonerProfile() {
               <div className="absolute -inset-1 bg-[#C89B3C]/20 rounded-full blur opacity-75 group-hover:opacity-100 transition duration-1000"></div>
               <div className="relative w-32 h-32 rounded-full overflow-hidden border-4 border-[#C89B3C]/20">
                 <Image
-                  src={`/api/profileIcon?iconId=${summoner?.profileIconId || 29}`}
+                  src={`https://ddragon.leagueoflegends.com/cdn/13.24.1/img/profileicon/${summoner?.profileIconId || 29}.png`}
                   alt="Summoner Icon"
                   width={128}
                   height={128}
                   className="object-cover"
-                  priority
+                  unoptimized
                 />
               </div>
             </div>
@@ -141,11 +141,12 @@ export default function SummonerProfile() {
                   >
                     <div className="w-16 h-16 rounded-lg bg-zinc-800/50 overflow-hidden">
                       <Image
-                        src={`/api/championIcon?championId=${match.champion}`}
+                        src={`https://ddragon.leagueoflegends.com/cdn/13.24.1/img/champion/${match.champion}.png`}
                         alt="Champion"
                         width={64}
                         height={64}
                         className="w-full h-full object-cover"
+                        unoptimized
                       />
                     </div>
                     <div className="flex-1">
